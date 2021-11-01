09f6ab3fba574fab5a892fcb636d5357537b8b69
mbakin <mehmetberkakinn@outlook.com>
2021-11-02 00:53:02 +0300

    create TodoAdd and TodoList components

--- src/components/TodoAdd.js
+++ src/components/TodoAdd.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import { useState } from "react";
+const API_BASE = "http://localhost:5000/api/todos/";
 
 const TodoAdd = () => {
-  return (
-    <div>
-      Test TodoAdd Component
-    </div>
-  )
-}
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
 
-export default TodoAdd
+  const handleTodo = (e) => {
+    setNewTodo(e.target.value);
+  };
+  const handleAddTodo = async () => {
+    const data = await fetch(API_BASE, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: newTodo,
+      }),
+    }).then((res) => res.json());
+
+    setTodos([...todos, data]);
+
+    setNewTodo("");
+  };
+  return (
+    <>
+      <input
+        type="text"
+        id="todoInputId"
+        data-testid="taskInput"
+        className="addTodoInput"
+        onChange={handleTodo}
+        value={newTodo}
+        placeholder="Write something.."
+      />
+
+      <button className="button" id="addTaskBtn" onClick={handleAddTodo}>
+        Create Task
+      </button>
+    </>
+  );
+};
+
+export default TodoAdd;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import { useState, useEffect } from "react";
 
-const TodoList = () => {
+const API_BASE = "http://localhost:5000/api/todos/";
+
+const TodoList = (props) => {
+
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    GetTodos();
+    
+  }, [todos]);
+
+  const GetTodos = async () => {
+    await fetch(API_BASE)
+      .then((res) => res.json())
+      .then((data) => setTodos(data))
+      .catch((err) => console.error("Error : ", err));
+  };
+  
   return (
     <div>
-      Test TodoList Component
+      <div className="todos" data-testid="allTodoList">
+        {todos.map((todo) => (
+          <div className="todo" data-testid="newTodo" name="newTodo" key={todo._id}>
+            <li id="todoItem">
+            <div className="text">{todo.title}</div>
+            <button className="delete-todo" >x</button>
+            </li>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
