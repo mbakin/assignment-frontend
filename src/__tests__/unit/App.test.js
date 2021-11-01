6818b9d18592d8612ea32867444d70fe59e7094e
mbakin <mehmetberkakinn@outlook.com>
2021-11-02 00:30:01 +0300

    create unit test, UI elements unit testing RED

---
+++ src/__tests__/unit/App.test.js
@@ -0,0 +1,32 @@
+import { render, screen } from "@testing-library/react";
+
+import App from "../../App";
+
+describe('App components render elements on screen', () => {
+  
+  test('renders Welcome User as a text', () => {
+    render(<App />)
+  
+    const welcomeUserElement = screen.getByText("Welcome User!")
+    expect(welcomeUserElement).toBeInTheDocument();
+  });
+  
+  test('renders Your Tasks as a text', () => {
+    render(<App />)
+
+    const yourTasksText = screen.getByText("Your Tasks")
+    expect(yourTasksText).toBeInTheDocument()
+  });
+
+
+
+  test("renders TodoAdd's element", () => {
+    render(<App />)
+    const queryByTestId = screen.getByTestId("taskInput")
+    const createTaskButton = screen.getByRole('button')
+       
+    expect(queryByTestId).toBeInTheDocument()
+    expect(createTaskButton).toBeInTheDocument()
+  })
+
+});
