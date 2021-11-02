016496e7c2edb4d4466450bdf06ef61556366919
mbakin <mehmetberkakinn@outlook.com>
2021-11-02 12:39:21 +0300

    create integration test TodoApp component

---
+++ src/__tests__/integration/AddTodo.test.js
@@ -0,0 +1,36 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+
+import TodoAdd from "../../components/TodoAdd";
+
+
+
+describe("if click 'Create Task' button", () => {
+
+  describe('with empty todos', () => {
+    test('does not trigger handleAddTodo function', () => {
+      render(<TodoAdd/>)
+      
+      const handleAddTodo = jest.fn()
+      const buttonElement = screen.getByRole('button')
+
+      fireEvent.click(buttonElement)
+      expect(handleAddTodo).not.toHaveBeenCalled()
+    })    
+  });
+  
+
+  describe('with data inside todo', () => {
+    test('triggers handleAddTodo function', () => {
+      const {queryByPlaceholderText} = render(<TodoAdd />)
+      
+      const buttonElement = screen.getByRole('button')
+      const addTodoInput = queryByPlaceholderText("Write something..")
+      const handleAddTodo = jest.fn()
+
+      fireEvent.change(addTodoInput, {target: {value: "buy some milk!"}})
+      fireEvent.click(buttonElement)
+      expect(handleAddTodo).not.toHaveBeenCalled()
+    })
+  });
+  
+});
